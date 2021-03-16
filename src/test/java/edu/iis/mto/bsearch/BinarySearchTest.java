2d2ba02a19e2aca6b778e3adcd83473e9db3a770
krecikovsky112 <krecikovsky112@gmail.com>
2021-03-16 11:42:46 +0100

    Testy_v1, execution of test assumptions

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -7,13 +7,68 @@ import org.junit.jupiter.api.Test;
 
 
 class BinarySearchTest {
+    private static final int[] ONE_ELEMENT_SEQUENCE = {2};
+    private static final int[] MULTI_ELEMENT_SEQUENCE = {-3, 0, 1, 3, 6, 9, 12};
 
     @BeforeEach
-    void setUp() throws Exception {}
+    void setUp() throws Exception {
+    }
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    public void searchForElementInSequence() {
+        int key = 2;
+        int expected_pos = 1;
+        SearchResult search = BinarySearch.search(key, ONE_ELEMENT_SEQUENCE);
+        assertTrue(search.isFound());
+        assertEquals(expected_pos, search.getPosition());
+    }
+
+    @Test
+    public void searchForElementNotInSequence() {
+        int key = 4;
+        int expected_pos = -1;
+        SearchResult search = BinarySearch.search(key, ONE_ELEMENT_SEQUENCE);
+        assertFalse(search.isFound());
+        assertEquals(expected_pos, search.getPosition());
+    }
+
+    @Test
+    public void searchForElementAtFirstPositionInSequence() {
+        int key = -3;
+        int expected_pos = 1;
+        SearchResult search = BinarySearch.search(key, MULTI_ELEMENT_SEQUENCE);
+        assertTrue(search.isFound());
+        assertEquals(expected_pos, search.getPosition());
+        assertEquals(MULTI_ELEMENT_SEQUENCE[search.getPosition() - 1], key);
+    }
+
+    @Test
+    void searchForElementAtLastPositionInSequence() {
+        int key = MULTI_ELEMENT_SEQUENCE[MULTI_ELEMENT_SEQUENCE.length - 1];
+        int expected_pos = MULTI_ELEMENT_SEQUENCE.length;
+        SearchResult search = BinarySearch.search(key, MULTI_ELEMENT_SEQUENCE);
+        assertTrue(search.isFound());
+        assertEquals(expected_pos, search.getPosition());
+        assertEquals(MULTI_ELEMENT_SEQUENCE[search.getPosition() - 1], key);
+    }
+
+    @Test
+    void searchForElementAtMiddlePositionInSequence() {
+        int key = 3;
+        int expected_pos = 4;
+        SearchResult search = BinarySearch.search(key, MULTI_ELEMENT_SEQUENCE);
+        assertTrue(search.isFound());
+        assertEquals(expected_pos, search.getPosition());
+        assertEquals(MULTI_ELEMENT_SEQUENCE[search.getPosition() - 1], key);
+    }
+
+    @Test
+    void searchForElementNotInMultiSequence() {
+        int key = 5;
+        int expected_pos = -1;
+        SearchResult search = BinarySearch.search(key, MULTI_ELEMENT_SEQUENCE);
+        assertFalse(search.isFound());
+        assertEquals(expected_pos, search.getPosition());
     }
 
 }
